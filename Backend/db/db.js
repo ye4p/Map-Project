25de1cb0c2b1ac8c1e119fd29af2f565e43e437b
Danila Golubkov <danilgolubkov172@gmail.com>
2025-09-22 20:24:09 -0400

    Connected local db to the backend

---
+++ Backend/db/db.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',      
+  host: 'localhost',      
+  database: 'places-db',   
+  password: process.env.DB_ACC, 
+  port: 5432,             
+});
+
+ pool.query('select * from test', (req,res) => {
+   return console.log(res)
+ })
+
+
+module.exports = pool;
