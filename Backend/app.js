03d2f9049700de037392c0ab83dcd9a92e2171ff
Danila Golubkov <danilgolubkov172@gmail.com>
2025-10-24 21:18:29 -0400

    fix

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ app.use(cors())
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, '../Frontend/dist')));
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../Frontend/dist', 'index.html'));
 });
 
