54ccfe764df4b5fb3dfed72e7d430d3d3df68540
Danila Golubkov <danilgolubkov172@gmail.com>
2025-11-14 21:42:30 -0500

    added analytics

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,7 +4,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Analytics } from "@vercel/analytics/react";
-
+import ReactGA from "react-ga4";
+ReactGA.initialize(import.meta.env.VITE_GA_ID)
+ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
 createRoot(document.getElementById('root')).render(
  // <StrictMode>
    <>
